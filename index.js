d7331312427e5211317a486083122c78e27ab030
hassansabbir <mahmoodsabbir3087@gmail.com>
2023-07-22 19:10:08 +0600

    load single college id

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5ymoa2u.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -32,6 +32,12 @@ async function run() {
     //colleges collection
     app.get("/colleges", async (req, res) => {
       const result = await collegesCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/details/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await collegesCollection.findOne(query);
       res.send(result);
     });
 
